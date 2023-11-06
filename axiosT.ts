6faf4255bf09f5dcb4f540ff74495260220dc83f
MoonSupport <jiwon3346@naver.com>
2023-11-06 22:43:57 +0900

    feat: body override

--- axiosT.ts
+++ axiosT.ts
@@ -1,33 +1,69 @@
 import axios, { AxiosInstance } from "axios";
 
 declare module "axios" {
-  interface AxiosInstance<T = any> {
+  interface AxiosInstance<T = any, D = any> {
     create<T>(config?: CreateAxiosDefaults): AxiosInstance<T>;
 
     interceptors: {
-      request: AxiosInterceptorManager<InternalAxiosRequestConfig>;
-      response: AxiosInterceptorManager<AxiosResponse<T, T>>;
+      request: AxiosInterceptorManager<InternalAxiosRequestConfig<D>>;
+      response: AxiosInterceptorManager<AxiosResponse<T, D>>;
     };
+
+    get<P = any, T = any, R = AxiosResponse<T>, D = any>(
+      url: string,
+      config?: MyAxiosRequestConfig<D, P>
+    ): Promise<R>;
   }
+
+  interface MyAxiosRequestConfig<D, P> extends AxiosRequestConfig<D> {
+    params?: P;
+  }
+
+  interface Axios {
+    get<P = any, T = any, R = AxiosResponse<T>, D = any>(
+      url: string,
+      config?: MyAxiosRequestConfig<D, P>
+    ): Promise<R>;
+  }
+  interface AxiosRequestConfig<D = any> {}
 }
 
-const magic = <T>() => {
-  const instance: AxiosInstance<T> = axios.create();
+const magic = <Params, T = any, D = any>(
+  buildParams: (params: Params) => string,
+  params: Params
+) => {
+  const instance: AxiosInstance<T, D> = axios.create();
+  const url = buildParams(params);
   return instance;
 };
 
-const instance = magic<{ id: 1 }>();
+const instance = magic<{ id: 1 }, { password: 2 }>((id) => "https://" + id, {
+  id: 1,
+});
+
+instance.interceptors.request.use(
+  function (request) {
+    request.params;
+    request.data?.password;
+    return request;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 instance.interceptors.response.use(
   function (response) {
     response.data;
-    // 2xx 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
-    // 응답 데이터가 있는 작업 수행
     return response;
   },
   function (error) {
-    // 2xx 외의 범위에 있는 상태 코드는 이 함수를 트리거 합니다.
-    // 응답 오류가 있는 작업 수행
     return Promise.reject(error);
   }
 );
+
+instance.get<{ body: 1 }>("", {
+  params: {
+    body: 1,
+  },
+});
